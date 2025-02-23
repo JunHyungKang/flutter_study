cf11d5c915d88f61d47644acdc1b9ccb1f9f6d53
jhkang <gogo0920007@gmail.com>
2025-02-23 19:37:43 +0900

    feat: floating action button 실습

--- lib/main.dart
+++ lib/main.dart
@@ -11,26 +11,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: GestureDetector(
-            onTap: (){
-              print('Tapped');
-            },
-            onDoubleTap: (){
-              print('Double Tapped');
-            },onLongPress: (){
-              print('Long Pressed');
-            },
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.red,
-              ),
-              width: 100,
-              height: 100,
-              ),
-              )
-            ),
-          ),
-        );
-      } 
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text('Floating Action Button'),
+        ),
+        body: Container()
+      ),
+    );
+  } 
 }   
