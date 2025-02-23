fc05090fb39a4da12f87da06e3ae2fac1a0ab510
jhkang <gogo0920007@gmail.com>
2025-02-23 18:34:19 +0900

    feat: text 위젯 확인

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        body: SizedBox(
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Code'),
-              Text('Factory'),
-            ],
+        body: Center(
+          child: Text('flutter study 어렵당',
+            style: TextStyle(
+              fontSize: 16.0,
+              color: Colors.blue,
+              // fontWeight: FontWeight.bold,
+              fontWeight: FontWeight.w700,
+            ),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
