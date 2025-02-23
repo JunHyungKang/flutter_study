f89625168c169772612198c643ee620588822b40
jhkang <gogo0920007@gmail.com>
2025-02-23 18:44:55 +0900

    feat: text button의 Text 위젯 설정 확인

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,10 +17,14 @@ class MyApp extends StatelessWidget {
             style: TextButton.styleFrom(
               foregroundColor: Colors.red,
             ),
-            child: Text('텍스트 버튼'),
+            child: Text('텍스트 버튼',
+            style: TextStyle(
+              fontSize: 30, // 원하는 폰트 크기로 변경
             ),
           ),
         ),
-      );
-    } 
-  }   
+      ),
+    ),
+    );
+  } 
+}   
